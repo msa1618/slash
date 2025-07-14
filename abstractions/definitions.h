dfaabe8733cabb0bf17471a6d39cc8cd521014f8
msa_1618 <laery62519@gmail.com>
2025-07-14 16:08:40 +0300

    abstractions: fix error where string_view was imported instead of string due to leftovers, and added background variables.

--- abstractions/definitions.h
+++ abstractions/definitions.h
@@ -5,7 +5,7 @@
 #define STDERR STDERR_FILENO
 #define STDIN STDIN_FILENO
 
-#include <string_view>
+#include <string>
 
 // ANSI colors
 
@@ -24,6 +24,20 @@ const std::string white     = "\x1b[37m";
 
 const std::string orange    = "\x1b[38;5;202m";
 const std::string gray      = "\x1b[38;5;237m";
+
+// Background colors
+
+const std::string bg_black   = "\x1b[40m";
+const std::string bg_red     = "\x1b[41m";
+const std::string bg_green   = "\x1b[42m";
+const std::string bg_yellow  = "\x1b[43m";
+const std::string bg_blue    = "\x1b[44m";
+const std::string bg_magenta = "\x1b[45m";
+const std::string bg_cyan    = "\x1b[46m";
+const std::string bg_white   = "\x1b[47m";
+
+const std::string bg_orange  = "\x1b[48;5;202m";
+const std::string bg_gray    = "\x1b[48;5;237m";
 
 
 
