f2b0f9c38173a76953896265e0d182e648f9f7f7
msa_1618 <laery62519@gmail.com>
2025-07-18 02:18:31 +0300

    ls: implement trees

--- src/slash-utils/ls.cpp
+++ src/slash-utils/ls.cpp
@@ -1,5 +1,6 @@
 #include "../command.h"
 #include "../abstractions/iofuncs.h"
+#include "../abstractions/info.h"
 
 #include <unistd.h>
 #include <fcntl.h>
@@ -9,17 +10,15 @@
 #include <math.h>
 
 class Ls : public Command {
-	public:
-		Ls() : Command("ls", "Lists all files, directories, and links in a directory", "") {}
-
-		int exec(std::vector<std::string> args) {
+	private:
+		int default_print(std::string dir_path) {
 			char buffer[512];
 			const char* path;
 
-			if (args.empty() || args[0].empty()) {
+			if (dir_path.empty()) {
 				path = getcwd(buffer, 512);
 			} else {
-				path = args[0].c_str();
+				path = dir_path.c_str();
 			}
 
 			DIR *d = opendir(path);
@@ -92,6 +91,110 @@ class Ls : public Command {
 
 			closedir(d);
 			return 0;
+		}
+
+		int tree_print(std::string dir_path, int indent = 2, int level = 0) {
+			DIR* dir = opendir(dir_path.c_str());
+			if(dir == 0) {
+				info::error(strerror(errno), errno, dir_path);
+				return -1;
+			}
+
+			struct dirent* entry;
+			while((entry = readdir(dir)) != nullptr) {
+				std::string name = entry->d_name;
+				if(name == "." || name == "..") continue;
+
+				std::stringstream fpath;
+				fpath << dir_path << "/" << name;
+				std::string path = fpath.str();
+
+				for(int i = 0; i < level; i++) {
+					io::print(std::string(indent, ' '));
+				}
+
+				if(entry->d_type == DT_REG) {// File
+					io::print(entry->d_name);
+					io::print("\n");
+					continue;
+				}
+
+				if(entry->d_type == DT_DIR) {
+					std::stringstream output;
+					output << blue << name << reset << "\n";
+					io::print(output.str());
+
+					tree_print(path, indent, level + 1);
+					continue;
+				}
+
+				if(entry->d_type == DT_LNK) {
+					char buffer[1024];
+					int bytesRead = readlink(path.c_str(), buffer, 1024);
+					if(bytesRead == -1) {
+						std::string error = std::string("Failed to read symlink target path: ") + strerror(errno);
+						info::error(error, errno, path);
+						continue;
+					}
+					buffer[bytesRead] = '\0';
+
+					std::stringstream symlink_output;
+					symlink_output << orange << name << reset << "->" << buffer << "\n";
+					io::print(symlink_output.str());
+				}
+		}
+
+		return 0;
+}
+
+	public:
+		Ls() : Command("ls", "Lists all files, directories, and links in a directory", "") {}
+
+		int exec(std::vector<std::string> args) {
+			if(args.empty()) {
+				char buffer[1024];
+				getcwd(buffer, 1024);
+
+				default_print(buffer);
+				return 0;
+			}
+
+			std::string path;
+			bool print_tree = false;
+
+			std::vector<std::string> validArgs = {
+				"-t", "-d"
+			};
+
+			for(auto& arg : args) {
+				if(!io::vecContains(validArgs, arg) && arg.starts_with('-')) {
+					info::error(std::string("Bad argument: ") + arg);
+					return -1;
+				}
+
+				if(arg == "-t") {
+					print_tree = true;
+				}
+
+				if(!arg.starts_with("-")) {
+					path = arg;
+				}
+			}
+
+			std::string dirpath = path;
+			if(path.empty()) {
+				char buffer[1024];
+				getcwd(buffer, 1024);
+				dirpath = buffer;
+			}
+
+			if(print_tree) {
+				tree_print(dirpath);
+				return 0;
+			} else {
+				default_print(dirpath);
+				return 0;
+			}
 		}
 };
 
