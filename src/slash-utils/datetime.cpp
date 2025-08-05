2b62cc675397dc4a9c7a0c2a760143b20efc053a
slash <aetheros@DESKTOP-HU6CECO.localdomain>
2025-08-05 17:18:46 +0300

    datetime: color colons aswell in help; subtle and minor update

--- src/slash-utils/datetime.cpp
+++ src/slash-utils/datetime.cpp
@@ -25,56 +25,56 @@ class Datetime : public Command {
 
 			std::vector<std::pair<std::string, std::string>> datetime_flags = {
 				// Dates
-				{"%Y", "Year (4 digits)"},
-				{"%y", "Year (2 digits)"},
-				{"%m", "Month (01–12)"},
-				{"%B", "Full month name"},
-				{"%b", "Abbreviated month name"},
-				{"%h", "Abbreviated month name (same as %b)"},
-				{"%d", "Day of month (01–31)"},
-				{"%e", "Day of month (1–31, space-padded)"},
-				{"%j", "Day of year (001–366)"},
-				{"%U", "Week number (Sunday as first day, 00–53)"},
-				{"%W", "Week number (Monday as first day, 00–53)"},
-				{"%C", "Century (year / 100)"},
-				{"%g", "ISO 8601 year (2-digit)"},
-				{"%G", "ISO 8601 year (4-digit)"},
-				{"%V", "ISO 8601 week number (01–53)"}, // 14th element
+				{"%Y:", "Year (4 digits)"},
+				{"%y:", "Year (2 digits)"},
+				{"%m:", "Month (01–12)"},
+				{"%B:", "Full month name"},
+				{"%b:", "Abbreviated month name"},
+				{"%h:", "Abbreviated month name (same as %b)"},
+				{"%d:", "Day of month (01–31)"},
+				{"%e:", "Day of month (1–31, space-padded)"},
+				{"%j:", "Day of year (001–366)"},
+				{"%U:", "Week number (Sunday as first day, 00–53)"},
+				{"%W:", "Week number (Monday as first day, 00–53)"},
+				{"%C:", "Century (year / 100)"},
+				{"%g:", "ISO 8601 year (2-digit)"},
+				{"%G:", "ISO 8601 year (4-digit)"},
+				{"%V:", "ISO 8601 week number (01–53)"}, // 14th element
 
 				// Time
-				{"%H", "Hour (00–23)"},
-				{"%I", "Hour (01–12)"},
-				{"%M", "Minute (00–59)"},
-				{"%S", "Second (00–60)"},
-				{"%p", "AM or PM"},
-				{"%r", "12-hour clock time (%I:%M:%S %p)"},
-				{"%R", "24-hour time (%H:%M)"},
-				{"%T", "24-hour time (%H:%M:%S)"},
-				{"%X", "Locale time representation"}, // 23rd element
+				{"%H:", "Hour (00–23)"},
+				{"%I:", "Hour (01–12)"},
+				{"%M:", "Minute (00–59)"},
+				{"%S:", "Second (00–60)"},
+				{"%p:", "AM or PM"},
+				{"%r:", "12-hour clock time (%I:%M:%S %p)"},
+				{"%R:", "24-hour time (%H:%M)"},
+				{"%T:", "24-hour time (%H:%M:%S)"},
+				{"%X:", "Locale time representation"}, // 23rd element
 
 				// Locale-based
-				{"%c", "Locale full datetime representation"},
-				{"%x", "Locale date representation"},
-				{"%D", "Date (%m/%d/%y)"},
-				{"%F", "Date (%Y-%m-%d, ISO 8601)"}, // 27th element
+				{"%c:", "Locale full datetime representation"},
+				{"%x:", "Locale date representation"},
+				{"%D:", "Date (%m/%d/%y)"},
+				{"%F:", "Date (%Y-%m-%d, ISO 8601)"}, // 27th element
 
 				// Weekdays
-				{"%A", "Full weekday name"},
-				{"%a", "Abbreviated weekday name"},
-				{"%u", "ISO weekday (1=Mon, 7=Sun)"},
-				{"%w", "Weekday (0=Sun, 6=Sat)"}, // 31st element
+				{"%A:", "Full weekday name"},
+				{"%a:", "Abbreviated weekday name"},
+				{"%u:", "ISO weekday (1=Mon, 7=Sun)"},
+				{"%w:", "Weekday (0=Sun, 6=Sat)"}, // 31st element
 
 				// Timezones
-				{"%Z", "Timezone abbreviation"},
-				{"%z", "UTC offset (+hhmm or -hhmm)"}, // 33rd element
+				{"%Z:", "Timezone abbreviation"},
+				{"%z:", "UTC offset (+hhmm or -hhmm)"}, // 33rd element
 
 				// slash-utils Special
-				{"%E", "Seconds since epoch"}, // 34th element
+				{"%E:", "Seconds since epoch"}, // 34th element
 
 				// Special
-				{"%%", "Literal %"},
-				{"%n", "Newline"},
-				{"%t", "Tab character"} // 37th element
+				{"%%:", "Literal %"},
+				{"%n:", "Newline"},
+				{"%t:", "Tab character"} // 37th element
 			};
 
 			auto print_flags = [datetime_flags](std::string title, std::string color, int starting_index, int ending_index){
@@ -86,7 +86,7 @@ class Datetime : public Command {
 					std::string flag = color + datetime_flags[i].first + reset;
 					std::string desc = datetime_flags[i].second;
 
-					io::print(flag + ": " + desc + "\n");
+					io::print(flag + " " + desc + "\n");
 				}
 
 				io::print("\n");
@@ -141,7 +141,7 @@ class Datetime : public Command {
 						print_formatted(args[++i]);
 					} else {
 						info::error("Flag \"-f\" requires a quote next to it");
-						return -1;
+						return EINVAL;
 					}
 				}
 			}
