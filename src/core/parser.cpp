2ff28fdcc78a135edba37a3746555cf4643cddc1
slash <aetheros@DESKTOP-HU6CECO.localdomain>
2025-08-06 01:23:15 +0300

    slash: add "colored strings"

--- src/core/parser.cpp
+++ src/core/parser.cpp
@@ -5,16 +5,48 @@
 #include "../builtin-cmds/alias.h"
 #include <regex>
 
+std::string bracket_to_ansi(std::string content) {
+    io::replace_all(content, "(red)", red);
+    io::replace_all(content, "(green)", green);
+    io::replace_all(content, "(yellow)", yellow);
+    io::replace_all(content, "(blue)", blue);
+    io::replace_all(content, "(magenta)", magenta);
+    io::replace_all(content, "(cyan)", cyan);
+    io::replace_all(content, "(white)", white);
+
+    io::replace_all(content, "(bg_red)", "\x1b[41m");
+    io::replace_all(content, "(bg_green)", "\x1b[42m");
+    io::replace_all(content, "(bg_yellow)", "\x1b[43m");
+    io::replace_all(content, "(bg_blue)", "\x1b[44m");
+    io::replace_all(content, "(bg_magenta)", "\x1b[45m");
+    io::replace_all(content, "(bg_cyan)", "\x1b[46m");
+    io::replace_all(content, "(bg_white)", "\x1b[47m");
+
+    io::replace_all(content, "(bold)", bold);
+    io::replace_all(content, "(italic)", "\x1b[3m");
+    io::replace_all(content, "(underline)", "\x1b[4m");
+    io::replace_all(content, "(blink)", "\x1b[5m");
+    io::replace_all(content, "(strikethrough)", "\x1b[9m");
+
+    io::replace_all(content, "(reset)", reset);
+
+    return content;
+}
+
 std::string remove_comments_outside_quotes(const std::string& input) {
     bool in_single_quote = false;
     bool in_double_quote = false;
 
+		bool color_quote_mode = false;
+
     for (size_t i = 0; i < input.size(); ++i) {
         char c = input[i];
+				char next;
+				if(i + 1 < input.size()) next = input[i + 1];
 
-        if (c == '\'' && !in_double_quote) {
+        if (c == '\'' && !in_double_quote && !color_quote_mode) {
             in_single_quote = !in_single_quote;
-        } else if (c == '"' && !in_single_quote) {
+        } else if (c == '"' && !in_single_quote && !color_quote_mode) {
             in_double_quote = !in_double_quote;
         }
 
@@ -32,11 +64,13 @@ Args parse_arguments(std::string command) {
 	command = io::trim(command);
 	command = remove_comments_outside_quotes(command);
 	if(command.empty()) return {};
+
 	std::string buffer;
 	Args args;
 
 	bool dq_mode = false;
 	bool sq_mode = false;
+	bool cq_mode = false;
 
 	// Detect and replace alias in the raw command string
 	std::string first_word;
@@ -53,75 +87,62 @@ Args parse_arguments(std::string command) {
 	buffer.clear();
 	for (i = 0; i < parsed_command.size(); i++) {
 		char c = parsed_command[i];
-		char next = parsed_command[i + 1];
+		char next = (i + 1 < parsed_command.size()) ? parsed_command[i + 1] : '\0';
 
-		if (c == ' ' && !buffer.empty() && !(dq_mode || sq_mode)) {
+		// Start of quoted string
+		if ((c == '"' || c == '\'') && !dq_mode && !sq_mode) {
+			if (i > 0 && parsed_command[i - 1] == '@') {
+				cq_mode = true;
+				// remove @ from buffer if it slipped in
+				if (!buffer.empty() && buffer.back() == '@')
+					buffer.pop_back();
+			}
+			if (c == '"') dq_mode = true;
+			else sq_mode = true;
+			continue;
+		}
+
+		// End of quoted string
+		if ((c == '"' && dq_mode) || (c == '\'' && sq_mode)) {
+			if (cq_mode) {
+				buffer = bracket_to_ansi(buffer);
+				cq_mode = false;
+			}
 			args.push_back(buffer);
 			buffer.clear();
-			continue;
-		}
-
-		if (c == '"' && buffer.empty() && !(dq_mode || sq_mode)) {
-			dq_mode = true;
-			continue;
-		}
-		if (c == '"' && dq_mode && !sq_mode) {
 			dq_mode = false;
-			args.push_back(buffer);
-			buffer.clear();
-			continue;
-		}
-
-		if (c == '\'' && buffer.empty() && !(dq_mode || sq_mode)) {
-			sq_mode = true;
-			continue;
-		}
-		if (c == '\'' && sq_mode && !dq_mode) {
 			sq_mode = false;
-			args.push_back(buffer);
-			buffer.clear();
 			continue;
 		}
 
-		if(c == '\\' && i + 1 < parsed_command.size()) {
-			switch(next) {
-				case '"': buffer.push_back('\"'); i++; break;
-				case '\'': buffer.push_back('\''); i++; break;
-				case 'n': buffer.push_back('\n'); i++; break;
-				case 't': buffer.push_back('\t'); i++; break;
-				case '\\': buffer.push_back('\\'); i++; break;
+		// Escapes
+		if (c == '\\' && next != '\0') {
+			switch (next) {
+				case '"': buffer.push_back('\"'); break;
+				case '\'': buffer.push_back('\''); break;
+				case 'n': buffer.push_back('\n'); break;
+				case 't': buffer.push_back('\t'); break;
+				case '\\': buffer.push_back('\\'); break;
 				default:
 					buffer.push_back(c);
 					buffer.push_back(next);
 					break;
 			}
+			i++; // skip next char
 			continue;
 		}
 
-		buffer.push_back(c);
-	}
-
-	if (!buffer.empty()) {
-		args.push_back(buffer);
-	}
-
-	// Variable substitution
-	for (auto& arg : args) {
-		size_t start = arg.find("$(");
-		if (start != std::string::npos) {
-			size_t end = arg.find(")", start);
-			if (end != std::string::npos) {
-				std::string variable = arg.substr(start + 2, end - (start + 2));
-				if (variable.find(" ") != std::string::npos) {
-					info::error("Variable cannot contain spaces!");
-					return {};
-				}
-				auto value = get_value(variable);
-				if (std::holds_alternative<std::string>(value)) {
-					arg.replace(start, end - start + 1, std::get<std::string>(value));
-				}
+		// Argument break
+		if (c == ' ' && !dq_mode && !sq_mode) {
+			if (!buffer.empty()) {
+				args.push_back(buffer);
+				buffer.clear();
 			}
+			continue;
 		}
+
+		// Fallback
+		buffer.push_back(c);
 	}
 
 	return args;
