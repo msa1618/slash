514c776ca5b1a9c76839b5f38e5dc51a4a6bf03a
slash <aetheros@DESKTOP-HU6CECO.localdomain>
2025-08-11 16:17:41 +0300

    main.cpp: fix accidentally hardcoded path

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@
 #define EXIT 5000
 
 int exec(std::vector<std::string> args, std::string raw_input, bool save_to_history) {
+	raw_input = io::trim(raw_input);
 	if(args.empty()) return 0;
 	if(args[0] == "exit") {
 		return EXIT;
@@ -21,6 +22,17 @@ int exec(std::vector<std::string> args, std::string raw_input, bool save_to_hist
 
 	if(args[0] == "cd") {
 		return cd(args);
+	}
+
+	bool bg = false;
+	bool devnull = false;
+	if(raw_input.ends_with("&n")) {
+    bg = true;
+    devnull = true;
+    args.pop_back();
+	} else if(raw_input.ends_with("&")) {
+			bg = true;
+			args.pop_back();
 	}
 
 	if(io::split(raw_input, ";").size() > 1) {
@@ -35,7 +47,7 @@ int exec(std::vector<std::string> args, std::string raw_input, bool save_to_hist
 	if(io::split(raw_input, "&&").size() > 1) {
 		for(auto cmd : io::split(raw_input, "&&")) {
 			cmd = io::trim(cmd);
-			int res = execute(parse_arguments(cmd), cmd, save_to_history);
+			int res = execute(parse_arguments(cmd), cmd, save_to_history, bg, devnull);
 			if (res == EXIT) return EXIT;
 			if (res != 0) break;
 		}
@@ -45,7 +57,7 @@ int exec(std::vector<std::string> args, std::string raw_input, bool save_to_hist
 	if(io::split(raw_input, "||").size() > 1) {
 		for(auto cmd : io::split(raw_input, "||")) {
 			cmd = io::trim(cmd);
-			int res = execute(parse_arguments(cmd), cmd, save_to_history);
+			int res = execute(parse_arguments(cmd), cmd, save_to_history, bg, devnull);
 			if (res == EXIT) return EXIT;
 			if (res == 0) break;
 		}
@@ -82,45 +94,44 @@ int exec(std::vector<std::string> args, std::string raw_input, bool save_to_hist
 		return redirect(new_args, command_part, save_to_history, path_part, append, is_stdout);
 	}
 
-	return execute(args, raw_input, save_to_history);
+	return execute(args, raw_input, save_to_history, bg, devnull);
 }
 
 
 int main(int argc, char* argv[]) {
-	signal(SIGINT, SIG_IGN);
+    signal(SIGINT, SIG_IGN);
+    atexit(disable_raw_mode);
 
-	auto cnt = get_json("/home/aetheros/.slash/config/prompts/default.json");
+		std::string HOME = getenv("HOME");
+    auto cnt = get_json(HOME + "/.slash/config/prompts/default.json");
 
-	if(argc > 1) {
-		std::vector<std::string> args;
-		args.reserve(argc - 1);
+    if(argc > 1) {
+        std::vector<std::string> args;
+        args.reserve(argc - 1);
 
-		for(int i = 1; i < argc; i++) {
-			args.emplace_back(argv[i]);
-		}
+        for(int i = 1; i < argc; i++) {
+            args.emplace_back(argv[i]);
+        }
 
-		auto original_args = args;
-		args = parse_arguments(io::join(args, " "));
+        auto original_args = args;
+        args = parse_arguments(io::join(args, " "));
 
-		exec(args, io::join(original_args, " "), true);
-		return 0;
-	}
-	
-	execute_startup_commands();
-	enable_raw_mode();
+        exec(args, io::join(original_args, " "), true);
+        return 0;
+    }
 
-	while(true) {
-		std::string input = print_prompt(cnt);
-		if(input.empty() || input.starts_with("#") || input.starts_with("//")) continue;
+    execute_startup_commands();
+    enable_raw_mode();
 
-		std::vector<std::string> args = parse_arguments(input);
+    while(true) {
+        std::string input = print_prompt(cnt);
+        if(input.empty() || input.starts_with("#")) continue;
 
-		if(exec(args, input, true) == EXIT) break;
-		enable_raw_mode();
+        std::vector<std::string> args = parse_arguments(input);
 
-		signal(SIGINT, SIG_IGN);
-	}
+        if(exec(args, input, true) == EXIT) break;
+    }
 
-	disable_raw_mode();
-	return 0;
+    disable_raw_mode();
+    return 0;
 }
